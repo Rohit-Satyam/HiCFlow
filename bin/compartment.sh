6b5fbd221a41c4f5935539ea3be7ec3f64818bac
Rohit Satyam <rohitsatyam102@gmail.com>
2024-05-05 23:09:17 +0300

    Add files via upload

--- bin/compartment.sh
+++ bin/compartment.sh
@@ -4,7 +4,7 @@ do
 	ls -1 $2/*.hic | while read p; 
 do  
 	n=$(echo $p | xargs -n 1 basename | cut -f 1 -d '.'); 
-	$3/tools/juicer-1.6/scripts/common/juicer_tools eigenvector KR $p \
+	$3/tools/juicer-1.6/scripts/common/juicer_tools eigenvector $5 $p \
 				${chr} BP $4 $2/${chr}_eigen.txt -p
 	done
 done < $1
